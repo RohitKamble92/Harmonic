f2aae41d3f2bf124403d71c22f588e76efa1e158
Rohit <rohitkamble10792@gmail.com>
2022-05-22 16:07:14 +0530

    Printed Harmonic Value

--- src/com/bridgelabz/harmonic/Harmonic.java
+++ src/com/bridgelabz/harmonic/Harmonic.java
@@ -1,7 +1,24 @@
 package com.bridgelabz.harmonic;
-
+import com.bridgelabz.util.Utility;
 public class Harmonic {
     public static void main(String[] args) {
-        System.out.println("Welcome to Harmonic number");
+        // INITIALIZATION
+        double result = 0, num = 0, j;
+        int n;
+        Utility u = new Utility();
+
+        // INPUT DATA
+        System.out.print("enter the harmonic you want to find: ");
+        n = u.inputInteger();
+
+        // COMPUTATION
+        for (j = 1; j <= n; j++) {
+            num = num + (1 / j);
+            result = num;
+        }
+        System.out.println("the value of harmonic no. " + n + " is: " + result);
+
     }
+
 }
+
